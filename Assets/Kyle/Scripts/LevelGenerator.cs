6a2632d7beea9719bc3ea7353b6c4f870dc45684
SAE\kbeaton <1021455@student.sae.edu.au>
2022-03-03 14:39:26 +1100

    Kyle push and setup

---
+++ Assets/Kyle/Scripts/LevelGenerator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] 
+    private Transform levelPart_1;
+
+
+
+
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
