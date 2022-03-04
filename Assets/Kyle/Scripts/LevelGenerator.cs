e326d0c170808cc1d6666a9428b301836314401e
Kysis321 <72172415+Kysis321@users.noreply.github.com>
2022-03-04 17:29:02 +1100

    Started Working on Level generation - basic tests done - Ssetting up endless generation in next few Iterations

--- Assets/Kyle/Scripts/LevelGenerator.cs
+++ Assets/Kyle/Scripts/LevelGenerator.cs
@@ -6,24 +6,45 @@ public class LevelGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] 
-    private Transform levelPart_1;
 
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 200F;
+
+    [SerializeField] private Transform levelPart_1;
+    [SerializeField] private Transform levelPart_Start;
+    [SerializeField] private GameObject player;
+
+    private Vector3 lastEndPosition;
+
+    private void Update()
+    {
+        //if (Vector3.Distance(player.transform.position, lastEndPosition);
+    }
 
 
 
     void Awake()
     {
+        lastEndPosition = levelPart_Start.Find("EndPosition").position;
+        SpawnLevelPart();
+
+        
+       
 
     }
-    void Start()
+
+    private void SpawnLevelPart()
     {
-        
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
     {
-        
+        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
+
+
 }
---
+++ Assets/Kyle/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //getting the input raw for use in FixedUpdate
+        horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Movment for the Charachter (Movemnent Crouch Jump)
+        controller.Move(horizontalMove *Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+
+    }
+
+
+}
