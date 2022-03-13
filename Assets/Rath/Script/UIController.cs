0647647ac23c3a55b9ef03b0626927cd7a6c074d
RS-SKM <81538660+RS-SKM@users.noreply.github.com>
2022-03-13 19:59:43 +1100

    Merging, resolved conflict

--- Assets/Rath/Script/UIController.cs
+++ Assets/Rath/Script/UIController.cs
@@ -15,12 +15,6 @@ public class UIController : MonoBehaviour
         distanceText = GameObject.Find("DistanceText").GetComponent<TextMeshProUGUI>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
