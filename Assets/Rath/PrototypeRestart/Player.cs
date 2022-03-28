01a7b1f006947f87a3234d057675df9a4756655c
RS-SKM <81538660+RS-SKM@users.noreply.github.com>
2022-03-28 11:45:49 +1100

    Prototype restarted, added temp death screen, obstacles , platform generation. paralax background
    Shooting was removed, will need to be reimplemented.

--- Assets/Rath/PrototypeRestart/Player.cs
+++ Assets/Rath/PrototypeRestart/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     public float gravity;
     public Vector2 velocity;
     public float maxXVelocity = 100;
@@ -15,14 +16,20 @@ public class Player : MonoBehaviour
     public bool isGrounded = false;
 
     public bool isHoldingJump = false;
-    public float maxHoldJumpTime = 0.3f;
+    public float maxHoldJumpTime = 0.4f;
+    public float maxMaxHoldJumpTime = 0.4f;
     public float holdJumpTimer = 0.0f;
 
     public float jumpGroundThreshold = 1;
 
+    public bool isDead = false;
+
+    public LayerMask groundLayerMask;
+    public LayerMask obstacleLayerMask;
+
     void Start()
     {
-        gravity = -0.5f;
+           
     }
 
     void Update()
@@ -40,29 +47,31 @@ public class Player : MonoBehaviour
                 holdJumpTimer = 0;
             }
         }
-        
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isHoldingJump = false;
         }
-    }
 
-    const float k_GroundedRadius = .2f;
-    [SerializeField] private LayerMask m_WhatIsGround;
-    [SerializeField] private Transform m_GroundCheck;
+
+
+    }
 
     private void FixedUpdate()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-        for (int i = 0; i < colliders.Length; i++)
+        if (isDead)
         {
-            if (colliders[i].gameObject != gameObject)
-            {
-                isGrounded = true;
-            }
+            return;
         }
 
         Vector2 pos = transform.position;
+
+        if (pos.y < -20)
+        {
+            isDead = true;
+            
+        }
+
         if (!isGrounded)
         {
             if (isHoldingJump)
@@ -74,6 +83,7 @@ public class Player : MonoBehaviour
                 }
             }
 
+
             pos.y += velocity.y * Time.fixedDeltaTime;
             if (!isHoldingJump)
             {
@@ -83,27 +93,38 @@ public class Player : MonoBehaviour
             Vector2 rayOrigin = new Vector2(pos.x + 0.7f, pos.y);
             Vector2 rayDirection = Vector2.up;
             float rayDistance = velocity.y * Time.fixedDeltaTime;
-            RaycastHit2D hit2D = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
-            /*if (hit2D.collider != null)
+            RaycastHit2D hit2D = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance, groundLayerMask);
+            if (hit2D.collider != null)
             {
                 Ground ground = hit2D.collider.GetComponent<Ground>();
                 if (ground != null)
                 {
-                    groundHeight = ground.groundHeight;
-                    pos.y = groundHeight;
-                    velocity.y = 0;
-                    isGrounded = true;
+                    if (pos.y >= ground.groundHeight)
+                    {
+                        groundHeight = ground.groundHeight;
+                        pos.y = groundHeight;
+                        velocity.y = 0;
+                        isGrounded = true;
+                    }
                 }
-            }*/
-            if (isGrounded)
-            {
-                Ground ground = hit2D.collider.GetComponent<Ground>();
-                groundHeight = ground.groundHeight;
-                pos.y = groundHeight;
-                velocity.y = 0;
-                isGrounded = true;
             }
             Debug.DrawRay(rayOrigin, rayDirection * rayDistance, Color.red);
+
+
+            Vector2 wallOrigin = new Vector2(pos.x, pos.y);
+            Vector2 wallDir = Vector2.right;
+            RaycastHit2D wallHit = Physics2D.Raycast(wallOrigin, wallDir, velocity.x * Time.fixedDeltaTime, groundLayerMask);
+            if (wallHit.collider != null)
+            {
+                Ground ground = wallHit.collider.GetComponent<Ground>();
+                if (ground != null)
+                {
+                    if (pos.y < ground.groundHeight)
+                    {
+                        velocity.x = 0;
+                    }
+                }
+            }
         }
 
         distance += velocity.x * Time.fixedDeltaTime;
@@ -112,6 +133,7 @@ public class Player : MonoBehaviour
         {
             float velocityRatio = velocity.x / maxXVelocity;
             acceleration = maxAcceleration * (1 - velocityRatio);
+            maxHoldJumpTime = maxMaxHoldJumpTime * velocityRatio;
 
             velocity.x += acceleration * Time.fixedDeltaTime;
             if (velocity.x >= maxXVelocity)
@@ -119,17 +141,49 @@ public class Player : MonoBehaviour
                 velocity.x = maxXVelocity;
             }
 
+
             Vector2 rayOrigin = new Vector2(pos.x - 0.7f, pos.y);
             Vector2 rayDirection = Vector2.up;
             float rayDistance = velocity.y * Time.fixedDeltaTime;
             RaycastHit2D hit2D = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
-            /*if (hit2D.collider == null)
+            if (hit2D.collider == null)
             {
                 isGrounded = false;
-            }*/
+            }
             Debug.DrawRay(rayOrigin, rayDirection * rayDistance, Color.yellow);
+
         }
+
+        Vector2 obstOrigin = new Vector2(pos.x, pos.y);
+        RaycastHit2D obstHitX = Physics2D.Raycast(obstOrigin, Vector2.right, velocity.x * Time.fixedDeltaTime, obstacleLayerMask);
+        if (obstHitX.collider != null)
+        {
+            Obstacle obstacle = obstHitX.collider.GetComponent<Obstacle>();
+            if (obstacle != null)
+            {
+                hitObstacle(obstacle);
+            }
+        }
+
+        RaycastHit2D obstHitY = Physics2D.Raycast(obstOrigin, Vector2.up, velocity.y * Time.fixedDeltaTime, obstacleLayerMask);
+        if (obstHitY.collider != null)
+        {
+            Obstacle obstacle = obstHitY.collider.GetComponent<Obstacle>();
+            if (obstacle != null)
+            {
+                hitObstacle(obstacle);
+            }
+        }
+
 
         transform.position = pos;
     }
+
+
+    void hitObstacle(Obstacle obstacle)
+    {
+        Destroy(obstacle.gameObject);
+        velocity.x *= 0.7f;
+    }
+
 }
